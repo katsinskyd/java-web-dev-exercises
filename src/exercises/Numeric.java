b5c76395f0661ba6cf34c0c1f11593011b70a5b0
Kat <kathryn.garnier@gmail.com>
2022-01-03 11:32:55 -0500

    completed exercise

---
+++ src/exercises/Numeric.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Numeric {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle: ");
+        double length = input.nextDouble();
+        System.out.println("Enter the width of the rectangle: ");
+        double width = input.nextDouble();
+        double area = length * width;
+        System.out.println("The area is " + area);
+    }
+}
