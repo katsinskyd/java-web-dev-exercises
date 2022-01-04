f7a88f37638ef61c5b33326993bb92e50b161d62
Kat <kathryn.garnier@gmail.com>
2022-01-03 20:49:48 -0500

    completed studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,26 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        boolean checkDouble = input.hasNextDouble();
+        double radius;
+
+        if (!checkDouble) {
+            System.out.println("Input not valid");
+        } else {
+            radius = input.nextDouble();
+
+            while (radius < 0) {
+                System.out.println("Input not valid, enter the non-negative radius of the circle: ");
+                radius = input.nextDouble();
+            }
+
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        }
+    }
+}
